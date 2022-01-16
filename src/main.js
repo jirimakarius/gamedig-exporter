a86c35ce9e529f5435b110becaf9223339eb60fe
Jiří Makarius <jiri.makarius.0@gmail.com>
2022-01-16 18:33:55 +0100

    Gamedig expoter init

---
+++ src/main.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const express = require('express');
+const server = express();
+const client = require('prom-client');
+const register = client.register;
+const Gamedig = require('gamedig');
+
+const maxplayers = new client.Gauge({
+	name: 'gamedig_max_players',
+	help: 'Max players',
+	labelNames: ['server_name', 'map', 'game'],
+});
+const players = new client.Gauge({
+	name: 'gamedig_players',
+	help: 'Number of players',
+	labelNames: ['server_name', 'map', 'game'],
+});
+const ping = new client.Gauge({
+	name: 'gamedig_ping',
+	help: 'Ping',
+	labelNames: ['server_name', 'map', 'game'],
+});
+
+async function collectGamedigData() {
+	const game_type = process.env.GAME_TYPE || 'protocol-valve';
+	const host = process.env.GAME_HOST || 'localhost';
+	const port = process.env.GAME_PORT || null;
+
+	const data = await Gamedig.query({ type: game_type, host, port });
+
+	const labels = Object.assign(
+		{ server_name: data.name, map: data.map },
+		collectGamedigRawData(game_type, data),
+	);
+	maxplayers.set(labels, data.maxplayers);
+	players.set(labels, data.players.length);
+	ping.set(labels, data.ping);
+}
+
+function collectGamedigRawData(game_type, data) {
+	switch (game_type) {
+		case 'protocol-valve':
+			return { game: data.raw.game };
+	}
+}
+
+server.get('/metrics', async (req, res) => {
+	try {
+		await collectGamedigData();
+		res.set('Content-Type', register.contentType);
+		res.end(await register.metrics());
+	} catch (ex) {
+		res.status(500).end(ex);
+	}
+});
+
+const port = process.env.PORT || 3000;
+console.log(
+	`Server listening to ${port}, metrics exposed on /metrics endpoint`,
+);
+server.listen(port);
